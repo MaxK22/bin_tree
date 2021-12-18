2d30759ee34d8c61b289535076c33ac71f62e294
MaxK <korotkovmaxim.a@gmail.com>
2021-12-18 10:50:24 +0300

    0.2

--- main.cpp
+++ main.cpp
@@ -12,24 +12,32 @@ public:
     int clones;
     tree *left;
     tree *right;
-    int add(int x);
+    tree *add(int x);
     tree(int x);
     void print();
     void print_leaves();
     bool balance();
-    void do_balance();
+    tree *do_balance();
     tree *rotate_right();
     tree *rotate_left();
+    void fix_h();
     tree *search(int x);
     tree *minimum();
-    tree *del(int x);
+    tree *remove(int x);
+    tree *remove_min();
     void print_tree();
 };
+
+void tree::fix_h() {
+    h = max((left ? left->h : 0) , (right ? right->h : 0))+1;
+}
 
 tree *tree::rotate_right() {
     tree *y = right;
     right = y->left;
     y->left = this;
+    this->fix_h();
+    y->fix_h();
     return y;
 }
 
@@ -37,11 +45,27 @@ tree *tree::rotate_left() {
     tree *y = left;
     left = y->right;
     y->right = this;
+    this->fix_h();
+    y->fix_h();
     return y;
 }
 
-void tree::do_balance() {
-    
+tree *tree::do_balance() {
+    tree * change = this;
+    if((left ? left->h : 0) - (right ? right->h : 0) == 2)
+    {
+        if(right != NULL && (right->left ? right->left->h : 0) > (right->right ? right->right->h : 0))
+            right = right->rotate_right();
+        change = rotate_left();
+    }
+    else if((left ? left->h : 0) - (right ? right->h : 0) == -2)
+    {
+        if(left != NULL && (left->right ? left->right->h : 0) > (left->left ? left->left->h : 0))
+            left = left->rotate_left();
+        change = rotate_right();
+    }
+    //h = max((left ? left->h : 0) , (right ? right->h : 0))+1;
+    return  change;
 }
 
 
@@ -53,42 +77,53 @@ tree::tree(int x) {
     h = 1;
     clones = 1;
 }
-int tree::add(int x) {
+tree *tree::add(int x) {
     if(x > src)
     {
         if(right != NULL)
-            h = max(right->add(x), h);
+            right = right->add(x);
         else
         {
             tree *new_element = new tree(x);
             right = new_element;
-            if(left == NULL)
-                h = 2;
         }
     }
     else if (x < src)
     {
         if(left != NULL)
-            h = max(left->add(x), h);
+            left = left->add(x);
         else
         {
             tree *new_element = new tree(x);
             left = new_element;
-            if(right == NULL)
-                h = 2;
         }
     }
     else
         clones++;
-    return h+1;
+    fix_h();
+    return do_balance();
 }
 
+tree *tree::search(int x) {
+    if(x > src)
+        return (right ? right->search(x) : NULL);
+    else if (x < src)
+        return (left ? left->search(x) : NULL);
+    else
+        return this;
+}
 
+tree *tree::minimum() {
+    tree * l = this;
+    while(l->left != NULL)
+        l = l->left;
+    return l;
+}
 
 void tree::print() {
     if(left != NULL)
         left->print();
-    cout << src << " " << clones << endl;
+    cout << src << " " << h << endl;
     if(right != NULL)
         right->print();
 }
@@ -113,17 +148,45 @@ bool tree::balance() {
         return true;
 }
 
+tree *tree::remove_min() {
+    if(!left)
+        return right;
+    left = left->remove_min();
+    return do_balance();
+}
+
+tree *tree::remove(int x) {
+    if(x < src)
+    {
+        left = (left ? left->remove(x) : NULL);
+    }
+    else if(x > src)
+    {
+        right = (right ? right->remove(x) : NULL);
+    }
+    else
+    {
+        tree *l = left;
+        tree *r = right;
+        delete this;
+    }
+    return do_balance();
+}
+
 int main() {
     int x;
     cin >> x;
     if(x != 0) {
-        tree a(x);
-        a.clones = 0;
+        tree *a = new tree(x);
+        a->clones = 0;
         while(x!=0) {
-            a.add(x);
+            a = a->add(x);
             cin >> x;
         }
-       a.print();
+       a->print();
+
+        cout << "test" << endl;
+
     }
     else
         cout << 0;
